3cd5f89519614f36aa174e36aceae3b053918315
Patil-Shrikrishna <krishnapt1993@gmail.com>
2023-09-15 13:37:45 +0530

    [feat] : implemented redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./store";
+import { persistor, store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </BrowserRouter>
 );
--- src/store.js
+++ src/store.js
@@ -1,6 +1,15 @@
 import { createStore } from "redux";
 import rootReducer from "./redux/reducer/rootReducer";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer);
+
+export const persistor = persistStore(store);
